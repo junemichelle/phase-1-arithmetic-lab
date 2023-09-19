5052992cc6e054d67d6012016336a8ec8724305e
michelle <junemichelle20@gmail.com>
2023-09-19 20:47:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+let num1=2
+let num2=31 
+const multiply= num1 * num2
+const random = Math.floor(Math.random() * 10) + 1;
+console.log("random number", random);
+let num3=16
+let num4=4
+const mod=num3/num4
+const max = Math.max(18,19,20)
